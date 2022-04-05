ba81d9ddea15f567445eca0560793e1b3e10daaa
Morgan <morgan.nott@hotmail.com>
2022-04-05 20:11:00 +0200

    - Ajout du fichier mock-pokemon.js

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 
 const port = 3000;
 
-app.get('/',
-    (req, res) => res.send('Hello Sam !'));
+app.get('/', (req, res) => res.send('Hello Sam !'));
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`Vous avez demandé le Pokemon N°${id}`);
+});
 
 app.listen(port,
     () => console.log(`Application Express démarré sur : http://localhost:${port}`));
