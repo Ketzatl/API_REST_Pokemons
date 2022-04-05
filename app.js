3dade455cd87a68ad03c1de230a7e9985112f81f
Morgan <morgan.nott@hotmail.com>
2022-04-05 19:29:27 +0200

    - Installation d'Express.js - Installation de Nodemon

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('pif');
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.get('/',
+    (req, res) => res.send('Hello Sam !'));
+
+app.listen(port,
+    () => console.log(`Application Express démarré sur : http://localhost:${port}`));
